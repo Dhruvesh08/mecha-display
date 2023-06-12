8796310a0c297e4a462ca38e924469cef231c133
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-12 17:35:07 +0530

    mecha-display-v2

--- src/display.rs
+++ src/display.rs
@@ -12,12 +12,12 @@ impl DisplayBrightness {
     }
 
     pub fn set_brightness(&self, brightness: u8) -> Result<(), Error> {
-        let adjusted_brightness = brightness * 255 / 100; // Convert 0-100 range to 0-255
+      
         let mut file = std::fs::File::create(&self.path)?;
-        println!("Setting brightness to: {} (input: {}) \n", adjusted_brightness, brightness);
-        println!("Writing to file: {}\n", self.path);
-        println!("Brightness: {}\n", adjusted_brightness);
-        file.write_all(adjusted_brightness.to_string().as_bytes())?;
+
+        println!("Set Brightness: {}", brightness);
+       
+        file.write_all(brightness.to_string().as_bytes())?;
         Ok(())
     }
 
@@ -29,8 +29,8 @@ impl DisplayBrightness {
             Ok(value) => value,
             Err(_) => return Err(Error::new(ErrorKind::InvalidData, "Invalid brightness value")),
         };
-        let adjusted_brightness = brightness * 100 / 255; // Convert 0-255 range to 0-100
-        Ok(adjusted_brightness)
+        println!("Get Brightness: {}", brightness);
+        Ok(brightness)
     }
 }
 
