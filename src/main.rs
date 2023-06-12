e2667288fcb290ed8f714eaabe205f3590a794a4
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-12 17:43:18 +0530

    user input brightness

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,24 @@ mod display;
 
 
 fn main() -> Result<(), Error> {
+
+    //get value form args
+    let args: Vec<String> = std::env::args().collect();
+    let brightness: u8 = match args[1].parse() {
+        Ok(value) => value,
+        Err(_) => {
+            return Err(Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid brightness value",
+            ))
+        }
+    };
     let display_brightness = DisplayBrightness::new();
-    display_brightness.set_brightness(50)?;
+    display_brightness.set_brightness(brightness)?;
 
     println!("Brightness: {}", display_brightness.get_brightness()?);
 
     //sleep for 0.5 seconds`
-    std::thread::sleep(std::time::Duration::from_millis(500));
 
-    display_brightness.set_brightness(100)?;
-    println!("Brightness: {}", display_brightness.get_brightness()?);
     Ok(())
 }
