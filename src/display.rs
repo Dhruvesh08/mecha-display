8496504b34ed5d5920a7891190c4f876ad7c36eb
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-12 17:39:45 +0530

    mecha-display-v2

--- src/display.rs
+++ src/display.rs
@@ -1,4 +1,4 @@
-use std::io::{Error, ErrorKind, Write, Read};
+use std::io::{Error, ErrorKind, Read, Write};
 
 pub struct DisplayBrightness {
     path: String,
@@ -12,11 +12,10 @@ impl DisplayBrightness {
     }
 
     pub fn set_brightness(&self, brightness: u8) -> Result<(), Error> {
-      
         let mut file = std::fs::File::create(&self.path)?;
 
-        println!("Set Brightness: {}", brightness);
-       
+        println!("Set Brightness: {}", brightness * (255 as u8) / 100 as u8);
+
         file.write_all(brightness.to_string().as_bytes())?;
         Ok(())
     }
@@ -27,11 +26,14 @@ impl DisplayBrightness {
         file.read_to_string(&mut buffer)?;
         let brightness: u8 = match buffer.trim().parse() {
             Ok(value) => value,
-            Err(_) => return Err(Error::new(ErrorKind::InvalidData, "Invalid brightness value")),
+            Err(_) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    "Invalid brightness value",
+                ))
+            }
         };
-        println!("Get Brightness: {}", brightness);
+        println!("Get Brightness: {}", brightness * (100 as u8) / 255 as u8);
         Ok(brightness)
     }
 }
-
-
